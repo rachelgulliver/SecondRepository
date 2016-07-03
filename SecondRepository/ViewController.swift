872011d27f9fcc1f6a30528d40fa9dd2a2e97894
rachelgulliver <rachelsgulliver@gmail.com>
2016-07-02 21:18:47 -0400

    Initial commit

---
+++ SecondRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondRepository
+//
+//  Created by Caleb Gulliver on 6/30/16.
+//  Copyright © 2016 RachelGulliver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
